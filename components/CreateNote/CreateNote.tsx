4cc3c3b69fde79235a8bcec6dbadf70a9112b14f
Iryna Buhela <developer.buhela@gmail.com>
2025-08-15 16:54:33 +0300

    fix

--- components/CreateNote/CreateNote.tsx
+++ components/CreateNote/CreateNote.tsx
@@ -1,13 +1,16 @@
-// src/components/CreateNote/CreateNote.tsx
 import NoteForm from "../NoteForm/NoteForm";
 import css from "./CreateNote.module.css";
 
-export default function CreateNote() {
+type CreateNoteProps = {
+  tags: string[];
+};
+
+export default function CreateNote({ tags }: CreateNoteProps) {
   return (
     <main className={css.main}>
       <div className={css.container}>
         <h1 className={css.title}>Create note</h1>
-        <NoteForm />
+        <NoteForm tags={tags} />
       </div>
     </main>
   );
