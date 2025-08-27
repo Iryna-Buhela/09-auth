ecc26b3c18a4e4e7fe03946a0960771b2efd193b
Iryna Buhela <developer.buhela@gmail.com>
2025-08-27 22:14:57 +0300

    api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,8 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+export type ApiError = AxiosError<{ error: string }>;
+
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const nextServer = axios.create({
   baseURL,
